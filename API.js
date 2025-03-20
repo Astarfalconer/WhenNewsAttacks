74d68e5eb34ede134c8c5eced9046ddffa97f39f
Joel Falconer <125147128+Astarfalconer@users.noreply.github.com>
2025-03-20 10:53:35 +0000

    Update API.js

--- API.js
+++ API.js
@@ -1,5 +1,5 @@
 const APIKEY = '427fecd4587a497f9c75a8eaa512defd';
-const date =  'from=2025-03-10&';
+const date =  'from=2025-03-15&';
 const query = "tesla";
 const queryRequest = `q=${query}&`;
 const url = `https://newsapi.org/v2/everything?${queryRequest}${date}apiKey=${APIKEY}`;
